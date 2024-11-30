f4e251a564bb8b04b5edfc004c47ebf8fb51e9ff
ashensavi <ashensavinda0@gmail.com>
2024-12-01 00:22:06 +0530

    fixed the taskstatus issue

--- script.js
+++ script.js
@@ -1,51 +1,65 @@
 
-let taskArr=[];
+let taskArr = [];
 
 function handleAddTask() {
     let task = document.getElementById("task-name").value;
     let desc = document.getElementById("task-desc").value;
-    let taskStatus = "To Do";
+    let taskStatus = 'To Do';
 
     let taskObj = {
         taskName: task,
         description: desc,
         taskStatus: taskStatus
     };
-    
+
     taskArr.push(taskObj);
     displayTasks();
 
     console.log(taskArr);
-    
+
     if (task === "") {
         alert('Please Enter a Task Name!');
         return;
     }
 
-    if(taskArr.length>3){
+    if (taskArr.length > 3) {
 
-        document.getElementById('scrollmsg').innerHTML='Scroll Down For More!';
+        document.getElementById('scrollmsg').innerHTML = 'Scroll Down For More!';
     }
-    
 
-   
-  
+
+
+
+}
+function isClicked(button) {
+    let card = button.closest('.card');
+    let taskIndex = Array.from(card.parentNode.children).indexOf(card); // Get index based on card position
+    let statusLabel = card.querySelector('.task-status');
+
+    // Update task status in the array
+    taskArr[taskIndex].taskStatus = "Done";
+
+    // Update the UI
+    statusLabel.textContent = "Done";
+    statusLabel.style.backgroundColor = "green";
+    statusLabel.style.color = "white";
 }
 
-function displayTasks(){
+
+function displayTasks() {
 
     let taskRow = document.getElementById('row-task');
     taskRow.innerHTML = "";
 
-    taskArr.forEach((taskObj, index) =>{
- 
-    let taskBody = `
+    taskArr.forEach((taskObj, index) => {
+
+        let taskBody = `
         <div class="col">
             <div class="card" style="width: 18rem; height: 20rem" >
                 <div class="card-body bg-black text-white">
                     <h5 class="card-title">Task : ${taskObj.taskName}</h5>
                     <p class="card-text">Description : ${taskObj.description}</p>
-                    <span class="task-status" style="background-color: #ffc107;">${taskObj.taskStatus}</span>
+                    <span class="task-status">${taskObj.taskStatus}</span>
                     <div class="row">
                      <div class="col">
                         <button class="btn-markasdone" onclick="isClicked(this)">Mark as Done</button>
@@ -60,8 +74,8 @@ function displayTasks(){
             </div>
         </div>`;
 
-    taskRow.innerHTML += taskBody;
-});
+        taskRow.innerHTML += taskBody;
+    });
 
 
 }
@@ -71,34 +85,15 @@ function deleteTask(index) {
     taskRow.innerHTML = "";
 
     taskArr.splice(index, 1);
-    displayTasks(); 
+    displayTasks();
 
     let taskDeleted = `
      <audio src="audio/deleted.mp3" autoplay></audio>
     `;
-    taskRow.innerHTML+=taskDeleted;
-
-   
-}
-   
+    taskRow.innerHTML += taskDeleted;
 
 
-
-function isClicked(button) {
-
-    let statusLabel = button.closest('.card-body').querySelector('.task-status');
-    
-  
-    statusLabel.textContent = "Done";
-    
-
-    statusLabel.style.backgroundColor = "green";
-    statusLabel.style.color = "white";
 }
 
-function deleteTask(index) {
-    taskArr.splice(index, 1);
-    displayTasks();
-}
 
 
